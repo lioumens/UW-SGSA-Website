d6b2b50dea7adad0b191c9dcbaf060fa0c15f8ba
alex hayes <alexpghayes@gmail.com>
2018-12-07 14:35:48 -0600

    Add materials for berg workshop

---
+++ workshops/berg/addUp.R
@@ -0,0 +1,7 @@
+            addUp_R<-function(n){
+                val=0
+                for (i in 1:n){
+                    val=val+i
+                }
+                return(val)
+            }
